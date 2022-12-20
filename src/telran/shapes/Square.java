bbc2e9abbd5783d01dff9cab4b3a6c180f13b882
user <user@192.168.1.194>
2022-12-20 18:11:12 +0200

    HW 15

--- src/telran/shapes/Square.java
+++ src/telran/shapes/Square.java
@@ -5,11 +5,11 @@ public class Square extends Rectangle {
 	public Square(int width) {
 		super(width, width);
 	}
-	
-public void setWidht(int width) {
+@Override		
+public void setWidth(int width) {
 	setSize(width);
 }
-
+	@Override
 public void setHeight(int width) {
 	setSize(width);
 }
@@ -18,20 +18,6 @@ private void setSize(int size) {
 	super.setWidth(size);
 	super.setHeight(size);
 }
-
-public int getWidth() {
-	return super.getWidth();
-}
-
-public int getHigth() {
-	return super.getWidth();
-}
-	
-public String[] presentation(int offset) {
-		
-	return super.presentation(offset);
-}
-
 
 
 }
