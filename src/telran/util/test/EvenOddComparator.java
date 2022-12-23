f18a904db4faf46c38459083175f2704002737be
user <user@192.168.1.194>
2022-12-23 19:20:05 +0200

    HW#16

---
+++ src/telran/util/test/EvenOddComparator.java
@@ -0,0 +1,20 @@
+package telran.util.test;
+
+import java.util.Comparator;
+
+public class EvenOddComparator implements Comparator<Integer> {
+
+	
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		int res = 0;
+		if (o1 % 2 == 0) {
+			res = (o2 % 2 == 0) ? o1 - o2 : -1;
+		} else {
+			res = (o2 % 2 == 0) ? 1 : o2 - o1;
+		}
+		return res;
+	}
+
+}
--- src/telran/util/test/MyArraysTest.java
+++ src/telran/util/test/MyArraysTest.java
@@ -21,19 +21,31 @@ class MyArraysTest {
 		assertArrayEquals(expected, strings);
 	}
 	
-	@Test //notMy!!!
-	void binarySearchTest() {
-		Integer[] numbers = { 1, 2, 3, 5, 6, 7, 8, 9 };
+	@Test 
+	@Disabled
+	void binarySearchStringTest() {
 		String[] strings = { "", "1", "23", "456", "7890" };
+		assertEquals(2, MyArrays.binarySearch(strings, "19", new StringLengthComparator()));
+		assertEquals(0, MyArrays.binarySearch(strings, "", new StringLengthComparator()));
+		assertEquals(-6, MyArrays.binarySearch(strings, "55555", new StringLengthComparator()));
 
-		//assertEquals(3, MyArrays.binarySearch(numbers, 5, new IntegerComparator()));
-		//assertEquals(-1, MyArrays.binarySearch(numbers, 0, new IntegerComparator()));
-		//assertEquals(-9, MyArrays.binarySearch(numbers, 10, new IntegerComparator()));
 		
-		assertEquals(2, MyArrays.binarySearch(strings, "32", new StringLengthComparator()));
-		//assertEquals(0, MyArrays.binarySearch(strings, "", new StringLengthComparator()));
-		//assertEquals(-6, MyArrays.binarySearch(strings, "55555", new StringLengthComparator()));
-
+	}
+	@Test
+	@Disabled
+	void binarySearchIntegerTest() {
+		Integer[] numbers = { 1, 2, 3, 5, 6, 7, 8, 9 };	
+		assertEquals(3, MyArrays.binarySearch(numbers, 5, new IntegerComparator()));
+		assertEquals(-1, MyArrays.binarySearch(numbers, 0, new IntegerComparator()));
+		assertEquals(-9, MyArrays.binarySearch(numbers, 10, new IntegerComparator()));
+	}
+	
+	@Test
+	void evenOddTest() {
+		Integer numbers[] = { 13, 2, -8, 47, 100, 10, 7 };
+		Integer expected[] = { -8, 2, 10, 100, 47, 13, 7 };
+		MyArrays.sort(numbers, new EvenOddComparator());
+		assertArrayEquals(expected, numbers);
 	}
 
 
